2c87cd786e592a071a97e947b344b21a8f7dff88
edwin-nealis <edwin.william.nealis@gmail.com>
2023-06-12 22:54:22 -0400

    changing chareters(i know) buttons work now

--- ChromeExtension/scripts/afterLogIn.js
+++ ChromeExtension/scripts/afterLogIn.js
@@ -1,4 +1,7 @@
 
+
+
+
 var manifest = chrome.runtime.getManifest();
 var apiKey = manifest.api_key;
 var memberShipId = "";
@@ -6,20 +9,30 @@ var membershipType;
 var charId1 = null;
 var charId2 = null;
 var charId3 = null;
-var selectedId;
 var token = null;
-var session;
-var selectedMode;
+const IORNBANNER = 19;
+const TRIALS = 84;
+const COMP = 69;
+const CONTROL = 10;
+
+var selectedId;
+var selectedMode = CONTROL;
 var charClass = new Array(3);
+var char1Stats = {};
+var char2Stats = {};
+var char3Stats = {};
+var teamMates = [];
+var enemys = [];
+var player = [];
+var games = [];
+var chars;
 window.onload = function () {
-    let displayDiv = document.getElementById('display');
     chrome.storage.local.get(['token'], async function (item) {
         token = JSON.parse(item['token']);
         let userJson;
         await getUser().then(json => {
             userJson = json;
         });
-        let p = document.createElement('p');
         console.log(userJson);
         memberShipId = userJson.Response.membershipId;
         let profileJson;
@@ -37,7 +50,7 @@ window.onload = function () {
             characterJson = json;
         });
         console.log(characterJson);
-        let chars = accountJson.Response.characters;
+        chars = accountJson.Response.characters;
         if (chars.length === 1) {
             charId1 = chars[0].characterId;
             charClass[0] = characterJson.Response.characters.data[charId1].classType;
@@ -54,82 +67,164 @@ window.onload = function () {
             charClass[1] = characterJson.Response.characters.data[charId2].classType;
             charClass[2] = characterJson.Response.characters.data[charId3].classType;
         }
-        
-        
+
+
         //character id used in all calls
-        selectedId = charId3;
-        selectedMode = 5
-        await getActivityIds(selectedMode, selectedId);
-        console.log(session);
-        var games = [];
+        selectedId = charId1;
+        let promisesActivitiesChar1 = [];
+        let promisesActivitiesChar2 = [];
+        let promisesActivitiesChar3 = [];
+        promisesActivitiesChar1.push(getActivityIds(IORNBANNER, charId1));
+        promisesActivitiesChar1.push(getActivityIds(TRIALS, charId1));
+        promisesActivitiesChar1.push(getActivityIds(COMP, charId1));
+        promisesActivitiesChar1.push(getActivityIds(CONTROL, charId1));
 
-        for (let i = 0; i < session.length; i++) {
-            await getPostGameReport(session[i].activityDetails.instanceId).then(json => {
-                games[i] = json;
-            });
+        promisesActivitiesChar2.push(getActivityIds(IORNBANNER, charId2));
+        promisesActivitiesChar2.push(getActivityIds(TRIALS, charId2));
+        promisesActivitiesChar2.push(getActivityIds(COMP, charId2));
+        promisesActivitiesChar2.push(getActivityIds(CONTROL, charId2));
 
-        }
-        console.log(games);
-        let teamMates = [];
-        let enemys = [];
-        let player = [];
+        promisesActivitiesChar3.push(getActivityIds(IORNBANNER, charId3));
+        promisesActivitiesChar3.push(getActivityIds(TRIALS, charId3));
+        promisesActivitiesChar3.push(getActivityIds(COMP, charId3));
+        promisesActivitiesChar3.push(getActivityIds(CONTROL, charId3));
 
-        for (let i = 0; i < games.length; i++) {
-            let team1 = new Array();
-            let team2 = new Array();
-            let team1Name = games[i].Response.teams[0].teamId;
-            let isPlayerTeam1 = null;
-            for (let ii = 0; ii < games[i].Response.entries.length; ii++) {
-                if (games[i].Response.entries[ii].values.team.basic.value === team1Name) {
-                    if (games[i].Response.entries[ii].characterId === selectedId) {
-                        player.push(games[i].Response.entries[ii]);
-                        isPlayerTeam1 = true;
-                    } else {
-                        team1.push(games[i].Response.entries[ii]);
-                    }
-                } else {
-                    if (games[i].Response.entries[ii].characterId === selectedId) {
-                        player.push(games[i].Response.entries[ii]);
-                        isPlayerTeam1 = false;
-                    } else {
-                        team2.push(games[i].Response.entries[ii]);
-                    }
-                }
-            }
-            if (i === 0) {
-                if (isPlayerTeam1) {
-                    teamMates = team1;
-                    enemys = team2;
-                } else {
-                    teamMates = team2;
-                    enemys = team1;
-                }
+        let activites = await Promise.all(promisesActivitiesChar1);
+        activites.forEach(json => {
+            char1Stats.charId = charId1;
+            let curentModes = json[0].activityDetails.modes;
+            if (curentModes.includes(IORNBANNER)) {
+                char1Stats[IORNBANNER] = json;
+            } else if (curentModes.includes(COMP)) {
+                char1Stats[COMP] = json;
+            } else if (curentModes.includes(CONTROL)) {
+                char1Stats[CONTROL] = json;
             } else {
-                if (isPlayerTeam1) {
-                    teamMates = teamMates.concat(team1);
-                    enemys = enemys.concat(team2);
-                } else {
-                    teamMates = teamMates.concat(team2);
-                    enemys = enemys.concat(team1);
-                }
+                char1Stats[TRIALS] = json;
             }
 
-        }
-        let p2 = document.createElement('p');
-        p2.textContent = "Games in Session: " + session.length;
-        let div = document.querySelector('#top');
-        div.append(p2);
+        });
+        activites = await Promise.all(promisesActivitiesChar2);
+        activites.forEach(json => {
+            char2Stats.charId = charId2;
+            let curentModes = json[0].activityDetails.modes;
+            if (curentModes.includes(IORNBANNER)) {
+                char2Stats[IORNBANNER] = json;
+            } else if (curentModes.includes(COMP)) {
+                char2Stats[COMP] = json;
+            } else if (curentModes.includes(CONTROL)) {
+                char2Stats[CONTROL] = json;
+            } else {
+                char2Stats[TRIALS] = json;
+            }
+
+        });
+        activites = await Promise.all(promisesActivitiesChar3);
+        activites.forEach(json => {
+            char3Stats.charId = charId3;
+            let curentModes = json[0].activityDetails.modes;
+            if (curentModes.includes(IORNBANNER)) {
+                char3Stats[IORNBANNER] = json;
+            } else if (curentModes.includes(COMP)) {
+                char3Stats[COMP] = json;
+            } else if (curentModes.includes(CONTROL)) {
+                char3Stats[CONTROL] = json;
+            } else {
+                char3Stats[TRIALS] = json;
+            }
+
+        });
+
+        console.log(char1Stats);
+        console.log(char2Stats);
+        console.log(char3Stats);
+        // await getActivityIds(selectedMode, selectedId).then(json => {
+        //     session = json;
+        // });
+
+        // console.log(session);
         let buttonContainer = document.createElement('div');
-        buttonContainer.setAttribute("id","button-container");
+        buttonContainer.setAttribute("id", "button-container");
+        let div = document.querySelector('#top');
         div.append(buttonContainer);
         createCharacterButtons(chars.length);
-        makePieGraph(enemys, session.length, "enemysChart", "Enemys", selectedMode);
-        makePieGraph(teamMates, session.length, "teamMatesChart", "teamMates", selectedMode);
-        makePieGraph(player, session.length, "playerChart", "players", selectedMode);
+        getSessionStats(selectedMode, selectedId);
 
     });
 }
+async function getSessionStats(mode, charId) {
+    let session;
+    let displayDiv = document.getElementById('session');
+    displayDiv.innerHTML = "";
+    if (char1Stats.charId === charId) {
+        session = char1Stats[mode];
+    } else if (char2Stats.charId === charId) {
+        session = char2Stats[mode];
+    } else {
+        session = char3Stats[mode];
+    }
+    console.log(session);
+    games = new Array();
 
+    for (let i = 0; i < session.length; i++) {
+        await getPostGameReport(session[i].activityDetails.instanceId).then(json => {
+            games[i] = json;
+        });
+
+    }
+    console.log(games);
+    teamMates = new Array();
+    enemys = new Array();
+    player = new Array();
+
+    for (let i = 0; i < games.length; i++) {
+        let team1 = new Array();
+        let team2 = new Array();
+        let team1Name = games[i].Response.teams[0].teamId;
+        let isPlayerTeam1 = null;
+        for (let ii = 0; ii < games[i].Response.entries.length; ii++) {
+            if (games[i].Response.entries[ii].values.team.basic.value === team1Name) {
+                if (games[i].Response.entries[ii].characterId === charId) {
+                    player.push(games[i].Response.entries[ii]);
+                    isPlayerTeam1 = true;
+                } else {
+                    team1.push(games[i].Response.entries[ii]);
+                }
+            } else {
+                if (games[i].Response.entries[ii].characterId === charId) {
+                    player.push(games[i].Response.entries[ii]);
+                    isPlayerTeam1 = false;
+                } else {
+                    team2.push(games[i].Response.entries[ii]);
+                }
+            }
+        }
+        if (i === 0) {
+            if (isPlayerTeam1) {
+                teamMates = team1;
+                enemys = team2;
+            } else {
+                teamMates = team2;
+                enemys = team1;
+            }
+        } else {
+            if (isPlayerTeam1) {
+                teamMates = teamMates.concat(team1);
+                enemys = enemys.concat(team2);
+            } else {
+                teamMates = teamMates.concat(team2);
+                enemys = enemys.concat(team1);
+            }
+        }
+
+    }
+    let p2 = document.createElement('p');
+    p2.textContent = "Games in Session: " + session.length;
+    displayDiv.append(p2);
+    makePieGraph(enemys, session.length, "enemysChart", "Enemys", mode);
+    makePieGraph(teamMates, session.length, "teamMatesChart", "teamMates", mode);
+    makePieGraph(player, session.length, "playerChart", "players", mode);
+}
 async function makePieGraph(players, games, chart, title, mode) {
     let xValues = ["primay", "special", "Heavy", "Grenade", "Melee", "Super", "Other Abilities"];
     let allKills = 0;
@@ -148,6 +243,9 @@ async function makePieGraph(players, games, chart, title, mode) {
     let itemTypeDisplayName = [];
     let promises = [];
     let quit = 0;
+    let canvas = document.getElementById(chart);
+    let context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
     var barColors = [
         "#b91d47",
         "#00aba9",
@@ -213,7 +311,12 @@ async function makePieGraph(players, games, chart, title, mode) {
     display.append(p3);
     display.append(p4);
     let yValues = [primaryKills, specialKills, heavyKills, grenadeKills, meleeKills, superKills, otherAbilities]
-
+    let oldChart = Chart.getChart(chart);
+    if (oldChart !== undefined) {
+        console.log(oldChart);
+        oldChart.data.datasets[0].data = yValues;
+        oldChart.update();
+       } else {
     new Chart(chart, {
         type: "pie",
         data: {
@@ -228,10 +331,10 @@ async function makePieGraph(players, games, chart, title, mode) {
         options: {
             plugins: {
                 legend: {
-                    position: "chartArea",
+                    position: "left",
                     labels: {
                         color: '#eceaea',
-                    } 
+                    }
                 }
             },
             title: {
@@ -257,6 +360,7 @@ async function makePieGraph(players, games, chart, title, mode) {
             },
         }
     });
+}
 
 }
 function getKd(charId, memId, memTypes, mode) {
@@ -369,40 +473,46 @@ function getAccount() {
 }
 //this always returns 2 matches even if the secound match is outside the time threshold 
 async function getActivityIds(mode, character) {
-    let activities;
-    await getActivityHistory(mode, character, 0)
-        .then(async acts => {
-            activities = acts.Response.activities
-            let lastInSession = 1;
-            let lastDate = new Date(activities[0].period);
-            let gotAllOfSession = false;
-            let i = 1
-            let pages = 0;
-            while (!gotAllOfSession) {
-                for (; i < activities.length; i++) {
-                    let thisDate = new Date(activities[i].period);
-                    let lastDatePluse2Hours = lastDate.getTime() - 7200000;
-                    if (thisDate.getTime() >= lastDatePluse2Hours) {
-                        lastInSession = i;
-                    } else {
-                        break;
+    return new Promise(async (resolve, reject) => {
+        let activities;
+        await getActivityHistory(mode, character, 0)
+            .then(async acts => {
+                activities = acts.Response.activities
+                let lastInSession = 1;
+                let lastDate = new Date(activities[0].period);
+                let gotAllOfSession = false;
+                let i = 1
+                let pages = 0;
+                while (!gotAllOfSession) {
+                    for (; i < activities.length; i++) {
+                        let thisDate = new Date(activities[i].period);
+                        let lastDatePluse2Hours = lastDate.getTime() - 7200000;
+                        if (thisDate.getTime() >= lastDatePluse2Hours) {
+                            lastInSession = i;
+                        } else {
+                            break;
+                        }
+                        lastDate = thisDate;
                     }
-                    lastDate = thisDate;
+                    console.log(lastInSession);
+                    if (lastInSession === activities.length - 1) {
+                        await getActivityHistory(mode, character, ++pages).then(json => {
+                            activities.concat(json.Response.activities)
+                        });
+                    } else {
+                        gotAllOfSession = true;
+                        activities.splice(lastInSession + 1);
+                    }
                 }
-                console.log(lastInSession);
-                if (lastInSession === activities.length - 1) {
-                    await getActivityHistory(mode, character, ++pages).then(json => {
-                        activities.concat(json.Response.activities)
-                    });
+                console.log(activities)
+                if (activities === 'unefined') {
+                    reject();
                 } else {
-                    gotAllOfSession = true;
-                    activities.splice(lastInSession + 1);
+                    resolve(activities);
                 }
-            }
-            console.log(activities)
 
-            session = activities;
-        });
+            });
+    });
 }
 
 function getActivityHistory(mode, charId, page) {
@@ -461,8 +571,8 @@ function createCharacterButtons(characterNum) {
             className = "Warlock";
         }
         button.innerHTML = className; // Find Class Name
-        button.setAttribute("id","button-character" + i);
-        button.onclick = function(){toggleButtons(this)};
+        button.setAttribute("id", "button-character" + i);
+        button.onclick = function () { toggleButtons(this) };
         document.querySelector('#button-container').appendChild(button);
 
     }
@@ -481,5 +591,9 @@ function toggleButtons(btn) {
         selectedId = charId3;
         console.log(charId3);
     }
-    // TODO: Call reload with id 
+    getSessionStats(selectedMode, selectedId);
+}
+
+function reload() {
+
 }
